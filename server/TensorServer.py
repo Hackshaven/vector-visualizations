9fff7f3e4bc1194f809979af64efa01de7bb4e68
Eric Hackathorn <eric.j.hackathorn@noaa.gov>
2020-05-18 17:08:18 -0600

    added shape endpoint to flask service

--- server/TensorServer.py
+++ server/TensorServer.py
@@ -30,6 +30,14 @@ def load_tensor(name, data_dir):
                     tensor_values = f.read(s0*s1*s2*3)
     return(tensor_values)
 
+def load_tensor_shape(name, data_dir):
+    with os.scandir(data_dir) as filelist:
+        for file in filelist:
+            if file.name == name and file.name.endswith(file_extension):
+                with open(file, "rb") as f:
+                    b = f.read(3)
+    return(b)
+
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
@@ -37,6 +45,20 @@ def index():
     
     welcome_string = "Tensor Server - WELCOME"
     return welcome_string
+
+@app.route('/shape', methods=['GET'])
+def get_shape():
+    
+    tensors = scan_datasets(data_directory_path)
+                    
+    name = request.args.get('name', None)
+    if name == None:
+        # return meta dict with all UVW databases
+        return jsonify(tensors)
+    
+    else:
+        # return RGB24 tuple values of specified tensor as byte stream
+        return load_tensor_shape(name, data_directory_path)
 
 @app.route('/tensor', methods=['GET'])
 def get_tensors():
